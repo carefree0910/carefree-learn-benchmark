03f1a7ee9f874dce369ce00133f406c1e21c0117
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-11-22 16:08:48 +0800

    🎉Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# carefree-learn-benchmark
+
+`carefree-learn-benchmark` is the benchmark for [carefree-learn](https://github.com/carefree0910/carefree-learn).
+
+
+## Citation
+
+If you use `carefree-learn-benchmark` in your research, we would greatly appreciate if you cite this library using this Bibtex:
+
+```
+@misc{carefree-learn-benchmark,
+  year={2020},
+  author={Yujian He},
+  title={carefree-learn-benchmark, the benchmark for carefree-learn},
+  howpublished={\url{https://https://github.com/carefree0910/carefree-learn-benchmark/}},
+}
+```
+
+
+## License
+
+`carefree-learn-benchmark` is MIT licensed, as found in the [`LICENSE`](https://github.com/carefree0910/carefree-learn-benchmark/blob/dev/LICENSE) file.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+DESCRIPTION = "Benchmark for `carefree-learn`"
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="carefree-learn-benchmark",
+    version=VERSION,
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["carefree-learn>=0.1.3"],
+    author="carefree0910",
+    author_email="syameimaru_kurumi@pku.edu.cn",
+    url="https://github.com/carefree0910/carefree-learn-benchmark",
+    download_url=f"https://github.com/carefree0910/carefree-learn-benchmark/archive/v{VERSION}.tar.gz",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    keywords="python carefree-learn PyTorch",
+)
